ca3b0f9b1c480182122dd654708c59b4e1b2b061
BuBiNo-MoB <emanuel.incarnato@gmail.com>
2024-07-02 16:36:48 +0200

    pagina per le distribuzioni visibile e funzionante, raccoglie i dati e posso cancellare

--- TuxDataF/src/app/pages/categories/distribution-list/distribution-list.component.ts
+++ TuxDataF/src/app/pages/categories/distribution-list/distribution-list.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { iDistribution } from '../../../models/distribution';
+import { AuthService } from '../../auth/auth.service';
+import { DistributionService } from '../../../services/distribution.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-distribution-list',
   templateUrl: './distribution-list.component.html',
   styleUrl: './distribution-list.component.scss'
 })
-export class DistributionListComponent {
+export class DistributionListComponent implements OnInit{
+  distributions: iDistribution[] = [];
+  isAdmin: boolean = false;
+  visibleDistributions: iDistribution[] = [];
 
+  constructor(private distributionService: DistributionService, private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.fetchDistributions();
+    this.authService.isAdmin$.subscribe({
+      next: isAdmin => {
+        this.isAdmin = isAdmin;
+      }
+    });
+  }
+
+  fetchDistributions(): void {
+    this.distributionService.getAll().subscribe({
+      next: (data: iDistribution[]) => {
+        this.distributions = data;
+        this.visibleDistributions = this.distributions.slice(-2);
+      },
+      error: (error: any) => {
+        console.error('Error fetching distributions', error);
+      }
+    });
+  }
+
+  viewDetails(id: number): void {
+    this.router.navigate(['/distributionDetails', id]);
+  }
+
+  deleteDistribution(id: number): void {
+    this.distributionService.deleteDistribution(id).subscribe({
+      next: () => {
+        this.distributions = this.distributions.filter(distro => distro.id !== id);
+        this.visibleDistributions = this.distributions.slice(0, 4); // Aggiorna le distribuzioni visibili
+      },
+      error: (error: any) => {
+        console.error('Error deleting distribution', error);
+      }
+    });
+  }
 }
